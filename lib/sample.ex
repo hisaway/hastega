953c299c6f7b93073e3f7dcd59c5d85c9999f1db
HISA WAY <z8mcb008@gmail.com>
2019-08-09 22:12:44 +0900

    add doc

--- lib/sample.ex
+++ lib/sample.ex
@@ -2,16 +2,37 @@ defmodule HastegaSample do
   import Hastega
   require Hastega
 
+  @moduledoc """
+  ```elixir
   defhastega do
     def cal(list) do
       list
       |> Enum.map(& &1 + 2)
+    |> Enum.map(fn x -> x * 2 end)
+  end
+  
+  #=>
+  def cal(list) do
+    list
+    |> HastegaNif.map_mult
+    |> HasegaNif.map_plus
+  end
+  ```
+  """
+  defhastega do
+    def list_mult_2(list) do
+      list
       |> Enum.map(fn x -> x * 2 end)
     end
 
-    def chunk_every(list) do
+    def list_plus_2(list) do
       list
-      |> Enum.chunk_every(4)
+      |> Enum.map(fn x -> x + 2 end)
     end
+
+    # def chunk_every(list) do
+    #   list
+    #   |> Enum.chunk_every(4)
+    # end
   end
 end
